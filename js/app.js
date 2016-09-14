c9a026544a822b69a95837177b50a3880a84ce72
Christoph Hoffmann <Christoph.Hoffmann@oeaw.ac.at>
2016-09-14 16:48:40 +0200

    initial commit for service module development stripped from all other issues

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 (function () {
   'use strict';
-  var app = angular.module('acdh', ['ngAria', 'ui.router','ngAnimate','ngSanitize','ngMaterial']);
+  var app = angular.module('acdh', ['ngAria', 'ui.router','ngAnimate','ngSanitize','ngMaterial', 'dhaAPIservice']);
   app.config(config);
   app.filter('unsafe', function($sce) { return $sce.trustAsHtml; });
   function config($stateProvider, $urlRouterProvider, $mdThemingProvider){
@@ -10,56 +10,6 @@
 		  url: '/:lang',
 		  templateUrl: 'js/views/start.html',
 		  controller: 'startCtrl'
-	  })
-	  .state('dha',{
-		  url: '/:lang/dha',
-		  templateUrl: 'js/views/navbar.html',
-		  controller: 'acdhNavCtrl'
-	  })
-	  .state('dha.newsevents',{
-		  url: '/newsevents',
-		  templateUrl: 'js/views/news.html',
-		  controller: 'newsCtrl'
-	  })
-	  .state('dha.s-news',{
-		  url: '/s-news/:nID',
-		  templateUrl: 'js/views/s-news.html',
-		  controller: 'singleEvCtrl'
-	  })
-	  .state('dha.partners',{
-		  url: '/partners',
-		  templateUrl: 'js/views/partners.html',
-		  controller: 'partnerCtrl'
-	  })
-	  .state('dha.s-partners',{
-		  url: '/s-partners/:nID',
-		  templateUrl: 'js/views/s-partners.html',
-		  controller: 'singlePaCtrl'
-	  })
-	  .state('dha.knowmore',{
-		  url: '/knowmore',
-		  templateUrl: 'js/views/knowmore.html',
-		  controller: 'knowmoreCtrl'
-	  })
-	  .state('dha.s-knowmore',{
-		  url: '/s-knowmore/:nID',
-		  templateUrl: 'js/views/s-knowmore.html',
-		  controller: 'singleKnCtrl'
-	  })
-	  .state('dha.projects',{
-		  url: '/projects',
-		  templateUrl: 'js/views/project.html',
-		  controller: 'projectCtrl'
-	  })
-	  .state('dha.s-project',{
-		  url: '/s-project/:nID',
-		  templateUrl: 'js/views/s-project.html',
-		  controller: 'singleProCtrl'
-	  })
-	  .state('dha.contact',{
-		  url: '/contact',
-		  templateUrl: 'js/views/contact.html',
-		  controller: 'contactCtrl'
 	  });
 	  $mdThemingProvider.theme('default')
 	  .primaryPalette('orange')
--- js/services.js
+++ js/services.js
@@ -1,42 +1,27 @@
-(function () {
-  'use strict';
-  var app = angular.module('acdh');
-  var listURL = {
-			   'start':'https://dhdev.eos.arz.oeaw.ac.at/__LANG__/api_0_1/nodes?parameters[field_tags]=209',
-			  'navbar':'https://dhdev.eos.arz.oeaw.ac.at/__LANG__/api_0_1/nodes?pagesize=all&parameters[type]=dha_page&parameters[tags]=214',
-		  'newsevents':'https://dhdev.eos.arz.oeaw.ac.at/__LANG__/api_0_1/nodes?pagesize=all&parameters[type]=event',
-			'partners':'https://dhdev.eos.arz.oeaw.ac.at/__LANG__/api_0_1/taxterm?parameters[vid]=5&parameters[tags]=207&pagesize=all',
-			'knowmore':'https://dhdev.eos.arz.oeaw.ac.at/__LANG__/api_0_1/nodes?pagesize=all&parameters[type]=biblio',
-			'projects':'https://dhdev.eos.arz.oeaw.ac.at/__LANG__/api_0_1/nodes?pagesize=all&parameters[type]=project',
-			 'contact':'https://dhdev.eos.arz.oeaw.ac.at/__LANG__/api_0_1/nodes?parameters[nid]=165',
- 
-		  'single':'https://dhdev.eos.arz.oeaw.ac.at/__LANG__/api_0_1/nodes?&parameters[nid]=',
-		 'singlep':'https://dhdev.eos.arz.oeaw.ac.at/__LANG__/api_0_1/taxterm?&parameters[tid]=',
-		   'termsflat':'https://dhdev.eos.arz.oeaw.ac.at/__LANG__/api_0_1/get_termstree?vid=4&flat=1'
-  };
-  /* Services */
-  app.factory('startList',['$http', '$stateParams',function($http,$stateParams){
-	  return { list: function(){
-			  var defLang = 'en';
-			  if(typeof($stateParams.lang) !== 'undefined'){ ($stateParams.lang.toLowerCase() == 'de') ? (defLang = $stateParams.lang.toLowerCase()) : (defLang = 'en'); }
-			  return $http.get(listURL['start'].replace('__LANG__', defLang)).then(function(res){return res;});
-			}
-	  };
-  }]);
-  app.factory('getList',['$http', '$stateParams',function($http,$stateParams){
-	  return { list: function(curState){ console.log('curState:', curState); console.log('$stateParams.lang:', $stateParams.lang);
-			  var defLang = 'en';
-			  if(typeof($stateParams.lang) !== 'undefined'){ ($stateParams.lang.toLowerCase() == 'de') ? (defLang = $stateParams.lang.toLowerCase()) : (defLang = 'en'); }
-			  return $http.get(listURL[curState].replace('__LANG__', defLang)).then(function(res){return res;});
-			}
-	  };
-  }]);
-  app.factory('getSingle',['$http', '$stateParams',function($http,$stateParams){
-	  return { one: function(curState,id){ console.log('curState:', curState);
-			  var defLang = 'en';
-			  if(typeof($stateParams.lang) !== 'undefined'){ ($stateParams.lang.toLowerCase() == 'de') ? (defLang = $stateParams.lang.toLowerCase()) : (defLang = 'en'); }
-			  return $http.get((listURL[curState].replace('__LANG__', defLang)) + id).then(function(res){return res;});
-			}
-	  };
-  }]);
-})();
+// the module in question should go here
+// code found here is only a bogy to make sure the controller and templates are set up fine
+// so structure within this model is entirely up to you, but it should be availaible from the controller
+// in the same way it is now
+
+var Config = {
+    "baseURL":"https://dhdev.eos.arz.oeaw.ac.at/",
+    "APIversion":"0.1",
+    "Language": "en",
+}
+
+var dhaAPIservice = angular.module('dhaAPIservice', []);
+
+dhaAPIservice.service('dhaAPIcall', ['$http', function($http){
+	var getNodes = function(fields){console.log('getNodes: ', fields);
+		return $http.get("https://dhdev.eos.arz.oeaw.ac.at/en/api_0_1/nodes?parameters[type]=event&parameters[field_dha_tags]=168&pagesize=all");
+	};
+	var getTerms = function(fields){console.log('getTerms: ', fields);
+	};
+	var setLanguage = function(language){console.log('setLanguage: ', language);
+	};
+	return {
+		getNodes:getNodes,
+		getTerms:getTerms,
+		setLanguage:setLanguage
+  	};
+}]);

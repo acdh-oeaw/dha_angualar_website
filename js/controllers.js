8e0efe58cb3cae2cdc026caf1a8dc2bb898ef32e
Christoph Hoffmann <Christoph.Hoffmann@oeaw.ac.at>
2017-03-03 14:51:54 +0100

    default view in news changed

--- js/controllers.js
+++ js/controllers.js
@@ -93,15 +93,15 @@ app.controller('startCtrl',['$rootScope','$scope','$http', '$state', '$statePara
 	TaxInit.then(function(res){
 		$rootScope.DHATax = res.data;
 		console.log(res.data);
-	}); 
+	});
 }])
 .controller('newsCtrl',['$rootScope','$scope','$http', '$state', '$stateParams','getContent',  function($rootScope, $scope, $http, $state, $stateParams, getContent){
 	$scope.Model = {};
-	$rootScope.uiview.current = "combined";
+	$rootScope.uiview.current = "tiles";
 	$rootScope.aviews = viewconfig[$state.current.name];
 	var curList = getContent.getNodes({"type":"event"});
 	curList.then(
-		function(res){ 
+		function(res){
 			for(var i=0; i<res.data.length; i++){
 				if( res.data[i].hasOwnProperty('schema:startDate') ){
 					//post-processing - this needs to go to a filter imho...
@@ -124,7 +124,7 @@ app.controller('startCtrl',['$rootScope','$scope','$http', '$state', '$statePara
 	$scope.currentSorting = "displayDate";
 	$scope.sortfield = "displayDate";
 	$scope.reverse = true;
-	$scope.selected = [];		
+	$scope.selected = [];
 	$scope.getNewOrder = function(a) {
 		if(a.slice(0,1) == "-") {$scope.reverse = true; $scope.sortfield = a.slice(1);}
   		else if(a.slice(0,1) != "-") {$scope.reverse = false; $scope.sortfield = a;}
@@ -141,7 +141,7 @@ app.controller('startCtrl',['$rootScope','$scope','$http', '$state', '$statePara
 		two: {
 			visible: false,
 			path: ""
-		},	
+		},
 		next:function(){
 			$scope.slideshow.current < $scope.mySingle[0]['schema:associatedMedia'].length - 1 ? $scope.slideshow.current++ : $scope.slideshow.current = 0;
 		},
@@ -163,7 +163,7 @@ app.controller('startCtrl',['$rootScope','$scope','$http', '$state', '$statePara
 		two: {
 			visible: false,
 			path: ""
-		},	
+		},
 		next:function(){
 			$scope.postergallery.current < $scope.mySingle[0]['attachment_previews'].length - 1 ? $scope.postergallery.current++ : $scope.postergallery.current = 0;
 		},
@@ -175,7 +175,7 @@ app.controller('startCtrl',['$rootScope','$scope','$http', '$state', '$statePara
 			var scrollBox = angular.element(document.querySelector( '.scrollcontainer' ));
 			scrollBox[0].scrollTop = "300";
 		}
-	}	
+	}
 	$rootScope.aviews = viewconfig[$state.current.name];
 	getContent.getInstitutions().then(function(res){
 		$scope.Institutions = res.data;
@@ -187,10 +187,10 @@ app.controller('startCtrl',['$rootScope','$scope','$http', '$state', '$statePara
 		center: {},
 		markers: {},
 		layers: {}
-	});		
+	});
 	curList.then(function(res){
 	  	$scope.mySingle = res.data;
-	  	//we're trusting the youtube link 
+	  	//we're trusting the youtube link
 	  	//TODO: proper validation, could be done in drupal
 	  	if($scope.mySingle[0]['schema:additionalType'] == 'schema:event' && $scope.mySingle[0]['schema:recordedIn']['url']) $scope.mySingle[0]['schema:recordedIn']['url'] = $sce.trustAsResourceUrl($scope.mySingle[0]['schema:recordedIn']['url']);
 	  	// if there's an adress, render a map with a marker
@@ -198,17 +198,17 @@ app.controller('startCtrl',['$rootScope','$scope','$http', '$state', '$statePara
 	  		$scope.mySingle[0]['geo'] = Geocoder.latLngForAddress($scope.mySingle[0]['schema:location']);
 	  		$scope.mySingle[0]['geo'].then(function(res){
 	  			console.log($scope.mySingle[0]['geo']);
-			  	leafletData.getMap().then(function(map) {  		
+			  	leafletData.getMap().then(function(map) {
 			    	map.setView(res, 16);
 			    	$scope.markers = {"venue":{"lat":res.lat, "lng": res.lng, "message":$scope.mySingle[0]['schema:location'],"focus":true}};
 			  		L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiZGhhc2l0ZSIsImEiOiJjaXZoN2lvN3EwMDVpMnRwZ3A1OHB3YWlkIn0.FTT1Ihj1_QSB-n8M4K1rbQ', {
 					    id: 'mapbox.light',
-					}).addTo(map);					    	
+					}).addTo(map);
 			    	map.invalidateSize();
 			    });
 				  $scope.$on('leafletDirectiveMap.resize', function(event){
 				      console.log(event);
-				  });					    
+				  });
 	  		});
 	  	}
 	  	//post-processing dates
@@ -216,7 +216,7 @@ app.controller('startCtrl',['$rootScope','$scope','$http', '$state', '$statePara
 			$scope.mySingle[0]['displayDate'] = parseInt($scope.mySingle[0]['schema:startDate'])*1000;
 		}
 		//slideshow
-		$scope.slideshow.one.path = $scope.mySingle[0]['schema:associatedMedia'][$scope.slideshow.current]['src'];		
+		$scope.slideshow.one.path = $scope.mySingle[0]['schema:associatedMedia'][$scope.slideshow.current]['src'];
 		$scope.$watch('slideshow.current', function() {
 			if($scope.slideshow.one.visible){
 				$scope.slideshow.two.path = $scope.mySingle[0]['schema:associatedMedia'][$scope.slideshow.current]['src'];
@@ -233,7 +233,7 @@ app.controller('startCtrl',['$rootScope','$scope','$http', '$state', '$statePara
 			else $scope.postergallery.one.path = $scope.mySingle[0]['attachment_previews'][$scope.postergallery.current]['src'];
 			$scope.postergallery.one.visible = !$scope.postergallery.one.visible;
 			$scope.postergallery.two.visible = !$scope.postergallery.two.visible;
-		});			
+		});
 	},
 	function(err){ console.log('err singleEvent: ', err); }
 	);
@@ -260,11 +260,11 @@ app.controller('startCtrl',['$rootScope','$scope','$http', '$state', '$statePara
 				res.data[i]['legalName'] = res.data[i]['schema:legalName'];
 				res.data[i]['parent'] = res.data[i]['schema:parentOrganization'];
 			}
-		}		
+		}
 		$scope.Institutions = res.data;
 	});
 	$rootScope.uiview.current = "list";
-	$rootScope.aviews = viewconfig[$state.current.name];		
+	$rootScope.aviews = viewconfig[$state.current.name];
 	//map not rendered for now
 		$scope.ATbounds = leafletBoundsHelpers.createBoundsFromArray([[ 49.02116, 9.53095  ],[ 46.37265,  20.16207 ]]); //creating austria bounds - maybe get coordinates from GeoNames as well?
 		angular.extend($scope, {
@@ -289,7 +289,7 @@ app.controller('startCtrl',['$rootScope','$scope','$http', '$state', '$statePara
 			map.invalidateSize();
 			window.onresize = setTimeout(function(){ map.invalidateSize();}, 400);
 	    });
-	///////////////////////////////		
+	///////////////////////////////
 	$rootScope.captions.then(function(res){
 		$scope.state = $state;
 		$scope.Model.navbar = res.data;
@@ -298,13 +298,13 @@ app.controller('startCtrl',['$rootScope','$scope','$http', '$state', '$statePara
 	$scope.currentSorting = "legalName";
 	$scope.sortfield = "legalName";
 	$scope.reverse = false;
-	$scope.selected = [];		
+	$scope.selected = [];
 	$scope.getNewOrder = function(a) {
 		if(a.slice(0,1) == "-") {$scope.reverse = true; $scope.sortfield = a.slice(1);}
   		else if(a.slice(0,1) != "-") {$scope.reverse = false; $scope.sortfield = a;}
 		console.log(a);
 	};
-	/////////////////////////////////////////////////////////////////				    
+	/////////////////////////////////////////////////////////////////
 }])
 .controller('embedTermCtrl',['$rootScope','$scope','$http', 'getContent', '$attrs',   function($rootScope, $scope, $http, getContent, $attrs){
 	$scope.myList = [];
@@ -321,7 +321,7 @@ app.controller('startCtrl',['$rootScope','$scope','$http', '$state', '$statePara
 			  console.log(res.data);
 			},
 			function(err){ console.log('err embedTermCtrl: ', err); }
-		);		
+		);
 	});
 }])
 .controller('singlePaCtrl',['$rootScope','$scope','$http', '$state', '$stateParams','getContent', 'Geocoder', 'leafletData', 'leafletBoundsHelpers', function($rootScope, $scope, $http, $state, $stateParams, getContent, Geocoder, leafletData, leafletBoundsHelpers){
@@ -329,7 +329,7 @@ app.controller('startCtrl',['$rootScope','$scope','$http', '$state', '$statePara
 	var bounds = leafletBoundsHelpers.createBoundsFromArray([[ 49.02116, 9.53095  ],[ 46.37265,  17.16207 ]]); //creating austria bounds - maybe get coordinates from GeoNames as well?
 	getContent.getInstitutions().then(function(res){
 		$scope.Institutions = res.data;
-	});	
+	});
 	angular.extend($scope, {
 		bounds: bounds,
 		center: {},
@@ -338,7 +338,7 @@ app.controller('startCtrl',['$rootScope','$scope','$http', '$state', '$statePara
 		mySingle: [],
 		evList: [],
 		proList: []
-	});			
+	});
 	var curList = getContent.getTerms({"tid": $stateParams.nID});
 	curList.then(
 		function(res){
@@ -361,19 +361,19 @@ app.controller('startCtrl',['$rootScope','$scope','$http', '$state', '$statePara
 		  		$scope.mySingle[0]['geo'] = Geocoder.latLngForAddress($scope.mySingle[0]['schema:address']);
 		  		$scope.mySingle[0]['geo'].then(function(res){
 		  			console.log($scope.mySingle[0]['geo']);
-				  	leafletData.getMap().then(function(map) {  		
+				  	leafletData.getMap().then(function(map) {
 				    	map.setView(res, 16);
 				    	$scope.markers = {"venue":{"lat":res.lat, "lng": res.lng, "message":$scope.mySingle[0]['schema:address'],"focus":true}};
 				  		L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiZGhhc2l0ZSIsImEiOiJjaXZoN2lvN3EwMDVpMnRwZ3A1OHB3YWlkIn0.FTT1Ihj1_QSB-n8M4K1rbQ', {
 						    id: 'mapbox.light',
-						}).addTo(map);					    	
+						}).addTo(map);
 				    	map.invalidateSize();
 				    });
 					  $scope.$on('leafletDirectiveMap.resize', function(event){
 					      console.log(event);
-					  });					    
+					  });
 		  		});
-		  	}				
+		  	}
 		},
 		function(err){ console.log('err singlePaCtrl: ', err); }
 	);
@@ -383,7 +383,7 @@ app.controller('startCtrl',['$rootScope','$scope','$http', '$state', '$statePara
 	$rootScope.uiview.current = "list";
 	$rootScope.aviews = viewconfig[$state.current.name];
 	var curList = getContent.getNodes({'type':'biblio'});
-	curList.then(function(res){ 
+	curList.then(function(res){
 		for(var i=0; i<res.data.length; i++){
 			if( res.data[i].hasOwnProperty('schema:headline') ){
 				res.data[i]['headline'] = res.data[i]['schema:headline'];
@@ -417,18 +417,18 @@ app.controller('startCtrl',['$rootScope','$scope','$http', '$state', '$statePara
 	$scope.currentSorting = "headline";
 	$scope.sortfield = "headline";
 	$scope.reverse = false;
-	$scope.selected = [];		
+	$scope.selected = [];
 	$scope.getNewOrder = function(a) {
 		if(a.slice(0,1) == "-") {$scope.reverse = true; $scope.sortfield = a.slice(1);}
   		else if(a.slice(0,1) != "-") {$scope.reverse = false; $scope.sortfield = a;}
 		console.log(a);
 	};
-	/////////////////////////////////////////////////////////////////		
+	/////////////////////////////////////////////////////////////////
 }])
 .controller('projectCtrl',['$rootScope','$scope','$http', '$state', '$stateParams','getContent',  function($rootScope, $scope, $http, $state, $stateParams, getContent){
 	getContent.getInstitutions().then(function(res){
 		$scope.Institutions = res.data;
-	});	
+	});
 	$scope.Model = {};
 	$rootScope.uiview.current = "tiles";
 	$rootScope.aviews = viewconfig[$state.current.name];
@@ -441,10 +441,10 @@ app.controller('startCtrl',['$rootScope','$scope','$http', '$state', '$statePara
 				}
 				if( res.data[i].hasOwnProperty('schema:sourceOrganization') ){
 					res.data[i]['source'] = res.data[i]['schema:sourceOrganization'][0].name;
-				}				
+				}
 			}
 			$scope.Model['projects'] = res.data;
-			
+
 		},
 		function(err){ console.log('err projectCtrl: ', err); }
 	);
@@ -456,7 +456,7 @@ app.controller('startCtrl',['$rootScope','$scope','$http', '$state', '$statePara
 	$scope.currentSorting = "headline";
 	$scope.sortfield = "headline";
 	$scope.reverse = false;
-	$scope.selected = [];		
+	$scope.selected = [];
 	$scope.getNewOrder = function(a) {
 		if(a.slice(0,1) == "-") {$scope.reverse = true; $scope.sortfield = a.slice(1);}
 			else if(a.slice(0,1) != "-") {$scope.reverse = false; $scope.sortfield = a;}
